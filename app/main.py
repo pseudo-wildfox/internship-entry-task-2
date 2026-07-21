8a515112b8540444be74ec29e8365b71882b2924
Timur Akhmetov <pseudo.wildfox@gmail.com>
2026-07-21 18:23:12 +0400

    feat: register new endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api.health import health
+from app.api.operations import router
 from app.db.database import check_connection
 
 
@@ -16,5 +17,5 @@ app = FastAPI(
 )
 
 app.include_router(router=health)
-
+app.include_router(router=router)
 
