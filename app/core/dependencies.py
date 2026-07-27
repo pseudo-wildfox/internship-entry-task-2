135cfddbdba7af18b3c8bd7c001190dabcaea4c1
Timur Akhmetov <pseudo.wildfox@gmail.com>
2026-07-27 18:40:10 +0400

    feat: add dependencies.py

---
+++ app/core/dependencies.py
@@ -0,0 +1,27 @@
+from fastapi import Depends
+
+from app.services.event_service import EventService
+from app.services.receipt_service import ReceiptService
+from app.services.send_job_service import SendJobService
+
+
+def get_send_job_service() -> SendJobService:
+    return SendJobService()
+
+
+def get_event_service() -> EventService:
+    return EventService()
+
+
+def get_receipt_service(
+    send_job_service: SendJobService = Depends(
+        get_send_job_service,
+    ),
+    event_service: EventService = Depends(
+        get_event_service,
+    ),
+) -> ReceiptService:
+    return ReceiptService(
+        send_job_service=send_job_service,
+        event_service=event_service,
+    )
